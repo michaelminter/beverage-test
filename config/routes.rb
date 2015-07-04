e5f4d4ce2760e992881c1551ebcecff452a85ed3
Michael Minter <mothore@gmail.com>
2015-07-04 17:43:11 -0500

    optimize resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
-  resources :orders
-  resources :customers
-  get 'home/index'
+  resources :orders, only: [:new, :create]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'home#index'
+  root 'orders#new'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
