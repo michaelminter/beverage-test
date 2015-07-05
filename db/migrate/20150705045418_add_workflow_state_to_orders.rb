77b67bfd5fd965a57862331dd135e6191c7cc397
Michael Minter <mothore@gmail.com>
2015-07-04 23:58:47 -0500

    set default workflow state

--- db/migrate/20150705045418_add_workflow_state_to_orders.rb
+++ db/migrate/20150705045418_add_workflow_state_to_orders.rb
@@ -1,5 +1,5 @@
 class AddWorkflowStateToOrders < ActiveRecord::Migration
   def change
-    add_column :orders, :workflow_state, :string, after: :quantity
+    add_column :orders, :workflow_state, :string, after: :quantity, default: 'ordered'
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -29,9 +29,9 @@ ActiveRecord::Schema.define(version: 20150705045418) do
   create_table "orders", force: :cascade do |t|
     t.integer  "customer_id",    limit: 4
     t.integer  "quantity",       limit: 4
-    t.string   "workflow_state", limit: 255
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
+    t.string   "workflow_state", limit: 255, default: "ordered"
+    t.datetime "created_at",                                     null: false
+    t.datetime "updated_at",                                     null: false
   end
 
 end
